f61a86807158242c63c570d31fa5b2f52cceab0c
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2025-01-14 19:51:06 -0500

    Intial models setup done

---
+++ src/courses/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+class AccessRequirement(models.TextChoices):
+    ANYONE = "any", "Anyone"
+    EMAIL_REQUIRED = "email_required", "Email Required"
+  
+
+class PublishStatus(models.TextChoices):
+    PUBLISHED = "publish", "PUBLISH"
+    COMING_SOON = "soon", "Coming Soon"
+    DRAFT = "draft", "DRAFT"
+
+def handle_upload(instance, filename):
+
+    return f"{filename}"
+
+class Course(models.Model) :
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    image= models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    access = models.CharField(
+        max_length=20,
+        choices= AccessRequirement.choices,
+        default=AccessRequirement.EMAIL_REQUIRED
+    )
+
+    status = models.CharField(
+        max_length=10,
+        choices= PublishStatus.choices,
+        default=PublishStatus.DRAFT
+    )
+
