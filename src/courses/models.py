16b8acd561f71972fc877e83edcd52359f1f390d
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2025-01-20 22:00:22 -0500

    Added Public id with uuid for each course

--- src/courses/models.py
+++ src/courses/models.py
@@ -22,18 +22,21 @@ def handle_upload(instance, filename):
 
     return f"{filename}"
 
-def get_public_id_prefix(instance, *args, **kwargs):
+def generate_public_id(instance, *args, **kwargs):
     title = instance.title
+    unique_id = str(uuid.uuid4()).replace("-", "")
+    if not title:
+        return unique_id
+    slug = slugify(title)
+    unique_id_short = unique_id[:5]
+    return f'Courses/{slug} - {unique_id_short}'
 
-    if title:
-        slug = slugify(title)
-        unique_id = str(uuid.uuid4()).replace("-", "")[:5]
-        return f'Courses/{slug} - {unique_id}'
-    
-    if instance.id: 
-        return f'courses/{instance.id}'
-    
-    return 'courses'
+
+def get_public_id_prefix(instance, *args, **kwargs):
+    public_id = instance.public_id
+    if not public_id: 
+        return "courses"
+    return f"courses/{public_id}"
 
 
 def get_display_name(instance, *args, **kwargs):
@@ -46,6 +49,7 @@ def get_display_name(instance, *args, **kwargs):
 class Course(models.Model) :
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
+    public_id = models.CharField(max_length=130, blank=True, null=True)
     image= CloudinaryField("image", null = True , 
            public_id_prefix = get_public_id_prefix, 
            display_name = get_display_name,
@@ -65,6 +69,13 @@ class Course(models.Model) :
 
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # before save 
+        if self.public_id == "" or self.public_id is None: 
+            self.public_id = generate_public_id(self)
+        super().save(*args, **kwargs)
+        # after save
 
     @property 
     def is_published(self):
